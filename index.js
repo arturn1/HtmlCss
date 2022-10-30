bd1bf29df9d8261a574d22261505b148edc05076
arturn1 <artur.a.patricio@gmail.com>
2022-10-29 21:38:10 -0300

    HtmlCss

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("OI");
+console.log(document);
+
+// const element = document.querySelector('#box4');
+// element.style.backgroundColor = 'red';
+
+document.getElementById("box4").style.color = "purple";
+
+// document.querySelector(".btn").addEventListener('click',
+//     () => { console.log("clicado") });
+
+// const button = document.querySelector('.btn');
+// button.addEventListener('click', () => {
+//     console.log("in");
+//     const element = document.querySelector('.box4');
+//     element.style.backgroundColor = 'red';
+// });
+
