6884876b1d274b8a24e257f52d443dd2cda055a7
Artur <Artur@DESKTOP-7TD6DU4>
2022-10-30 00:34:18 -0300

    ByWindows

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ console.log(document);
 // const element = document.querySelector('#box4');
 // element.style.backgroundColor = 'red';
 
-document.getElementById("box4").style.color = "purple";
+document.getElementById("box4").style.backgroundColor = "red";
 
 // document.querySelector(".btn").addEventListener('click',
 //     () => { console.log("clicado") });
 
-// const button = document.querySelector('.btn');
-// button.addEventListener('click', () => {
-//     console.log("in");
-//     const element = document.querySelector('.box4');
-//     element.style.backgroundColor = 'red';
-// });
+const button = document.querySelector('.btn-box2');
+
+button.addEventListener('click', () => {
+    console.log("in");
+    document.querySelector(".box4").style.transition = "2s";
+    document.getElementById("box4").style.transition = "2s";
+    document.querySelector('.box4').style.backgroundColor = 'red';
+    document.querySelector('#box4').style.backgroundColor = 'purple';
+    button.classList.toggle("btn-hover")
+});
+
+const btn = document.querySelector("#btn-box3");
+btn.addEventListener("click", () => btn.classList.toggle("toogle-color"));
 
